b5de1a761990ced2f634a294058367519e73b8c4
Hhorowitz734 <hhorowitz734@gmail.com>
2025-04-03 16:52:00 -0500

    made main.rs expose endpoints

--- src/judge/src/main.rs
+++ src/judge/src/main.rs
@@ -10,30 +10,23 @@ mod models;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
-    // Load in env
     dotenv().ok();
+
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    // Set up connection pool
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&db_url)
         .await
         .expect("Could not connect to the database");
 
-    
-    // Start HttpServer
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
             .configure(routes::config)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))? // <-- publicly accessible
     .run()
     .await
-
-
-
 }
 
