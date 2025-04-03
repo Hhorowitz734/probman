1e20fb3f91c98c00f9dc80bf351a5e7c7ffb2bb0
Hhorowitz734 <hhorowitz734@gmail.com>
2025-04-03 15:19:03 -0500

    wrote routes + unit tests for submissions

--- src/models/test_case.rs
+++ src/models/test_case.rs
@@ -1,10 +1,14 @@
+// src/models/test_case.rs
+
 use serde::Serialize;
 use sqlx::FromRow;
+use uuid::Uuid;
+
 
 #[derive(Debug, FromRow, Serialize)]
 pub struct TestCase {
-    pub id: i32,
-    pub problem_id: String,
+    pub id: Uuid,
+    pub problem_id: Uuid,
     pub input: String,
     pub expected_output: String,
     pub visibility: Option<String>,
